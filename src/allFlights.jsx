ade26f28945f23339b85eb86d37389b43ce8261a
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-03-08 19:45:46 +0100

    modified:   src/allFlights.jsx 	renamed:    flightdata.json -> src/flightdata.json 	deleted:    src/flights.jsx

--- src/allFlights.jsx
+++ src/allFlights.jsx
@@ -1,9 +1,97 @@
+import { useEffect, useState } from "react";
+import flightDataFile from "./flightdata.json";
 const allFlights = () => {
+  //[] means initialised with an empty array
+  const [displayFlights, setDisplayFlights] = useState([]);
+
+  useEffect(() => {
+    FetchFlights();
+  }, []);
+
+  const FetchFlights = async () => {
+    try {
+      const response = await fetch(flightDataFile);
+      const data = await response.json();
+      setDisplayFlights(data);
+    } catch (error) {
+      console.log("Error fetching data: ", error);
+    }
+  };
+
   return (
     <>
       <h2>All flights go here</h2>
+      <div>{displayFlights}</div>
     </>
   );
 };
 
 export default allFlights;
+
+// import { useState, useEffect } from "react";
+
+// function Flights() {
+//   const [jobs, setJobs] = useState([]);
+//   const [selectedCompany, setSelectedCompany] = useState(null);
+
+//   useEffect(() => {
+//     fetchJobs();
+//   }, []);
+
+// const fetchJobs = async () => {
+//   try {
+//     const response = await fetch("https://course-api.com/react-tabs-project");
+//     const data = await response.json();
+//     setJobs(data);
+// } catch (error) {
+//   console.error("Error fetching data:", error);
+// }
+// };
+
+// const handleCompanyClick = (company) => {
+//   setSelectedCompany(company);
+// };
+
+// return (
+//   <>
+//     <div className="logistics-table">
+//       <h1>Jobs</h1>
+//       <div className="">
+//         <ul className="flex">
+//           {jobs.map((job, index) => (
+//             <li
+//               className="logistics-table li selected blue"
+//         key={index}
+//         onClick={() => handleCompanyClick(job.company)}
+//       >
+//         {job.company}
+//       </li>
+//     ))}
+//   </ul>
+// </div>
+// {selectedCompany && (
+//   <div>
+//     <h2>{selectedCompany}</h2>
+//     <div>
+//       {jobs
+//                 .filter((job) => job.company === selectedCompany)
+//                 .map((job, index) => (
+//                   <article key={index}>
+//                     <h3>{job.title}</h3>
+//                     <p>{job.dates}</p>
+//                     <ul>
+//                       {job.duties.map((duty, index) => (
+//                         <li key={index}>{duty}</li>
+//                       ))}
+//                     </ul>
+//                   </article>
+//                 ))}
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Flights;
