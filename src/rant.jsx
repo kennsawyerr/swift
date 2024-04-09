2a563ab9ad080c648d07fb358440be5e3bae047a
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-04-09 11:27:56 +0100

    modified:   src/App.css 	modified:   src/rant.jsx

--- src/rant.jsx
+++ src/rant.jsx
@@ -1,36 +1,34 @@
 import { useState, useEffect } from "react";
 
 const Rant = () => {
-  const [meal, setMeal] = useState(null);
-  //www.themealdb.com/api/json/v1/1/random.php
+  const [meals, setMeals] = useState([]);
+
   useEffect(() => {
     fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert")
       .then((response) => response.json())
       .then((data) => {
-        setMeal(data.meals[0]); // Assuming the API returns an array of meals
+        const randomMeals = getRandomMeals(data.meals, 10);
+        setMeals(randomMeals);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
+  const getRandomMeals = (mealsArray, count) => {
+    const shuffled = mealsArray.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+  };
+
   return (
     <div>
-      {meal && (
-        <div>
+      {meals.map((meal) => (
+        <div key={meal.idMeal}>
           <h2>{meal.strMeal}</h2>
           <img src={meal.strMealThumb} alt={meal.strMeal} />
           <p>{meal.strCategory}</p>
         </div>
-      )}
-
-      {meal && (
-        <div>
-          <h2>{meal.strMeal}</h2>
-          <img src={meal.strMealThumb} alt={meal.strMeal} />
-          <p>{meal.strCategory}</p>
-        </div>
-      )}
+      ))}
     </div>
   );
 };
